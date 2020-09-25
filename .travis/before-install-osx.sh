3341fe8c471455dc87365351d145dee6987652cc
Marcin Paczkowski <dyfeer@gmail.com>
2020-09-24 19:54:50 -0700

    add option to update homebrew

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -2,12 +2,16 @@
 
 export HOMEBREW_NO_ANALYTICS=1
 
+if ! $UPDATE_HOMEBREW; then
+   export HOMEBREW_NO_AUTO_UPDATE=1
+fi
+
 brew install libsndfile || brew install libsndfile || exit 1
 brew install portaudio || exit 2
 brew install ccache || exit 3
 if  [[ ! -z "$QT_FORMULA" ]]; then
     brew install $QT_FORMULA --force || exit 4
-else
+elif $UPDATE_HOMEBREW; then
     brew upgrade qt5 || exit 4
 fi
 brew link qt5 --force || exit 5
