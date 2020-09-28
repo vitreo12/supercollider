8fd5b3b8b5754e2d80ff6690e8e9464693e3f67b
Marcin Paczkowski <dyfeer@gmail.com>
2020-09-27 18:43:44 -0700

    rework deployment variables

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -3,5 +3,5 @@
 mkdir -p $HOME/artifacts
 
 cd $TRAVIS_BUILD_DIR/BUILD/Install
-zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
-cp $HOME/artifacts/SC-$TRAVIS_COMMIT.zip $HOME/SuperCollider-$VERSION_TO_BUILD-macOS.zip
+zip -q -r --symlinks $HOME/artifacts/$ARTIFACT_AWS_NAME.zip SuperCollider
+cp $HOME/artifacts/$ARTIFACT_AWS_NAME.zip $HOME/$ARTIFACT_GH_NAME.zip
