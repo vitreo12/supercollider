78e30d09baa3ee565caf8603ed7ff02fd84fcb55
Marcin Paczkowski <dyfeer@gmail.com>
2020-10-01 21:00:04 -0700

    Update package-osx.sh

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -3,5 +3,5 @@
 mkdir -p $HOME/artifacts
 
 cd $TRAVIS_BUILD_DIR/BUILD/Install
-zip -q -r --symlinks $HOME/artifacts/$ARTIFACT_AWS_NAME.zip SuperCollider
-cp $HOME/artifacts/$ARTIFACT_AWS_NAME.zip $HOME/$ARTIFACT_GH_NAME.zip
+zip -q -r --symlinks $HOME/artifacts/$S3_ARTIFACT_NAME.zip SuperCollider
+cp $HOME/artifacts/$S3_ARTIFACT_NAME.zip $HOME/$GITHUB_ARTIFACT_NAME.zip
