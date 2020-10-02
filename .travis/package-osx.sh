02f106c1a0c2bec40dd96c22bb25d982a0f18ef0
Marcin Paczkowski <dyfeer@gmail.com>
2020-10-02 14:57:40 -0700

    debug

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -3,5 +3,8 @@
 mkdir -p $HOME/artifacts
 
 cd $TRAVIS_BUILD_DIR/BUILD/Install
+ls -a
 zip -q -r --symlinks $HOME/artifacts/$S3_ARTIFACT_NAME.zip SuperCollider
+ls -a $HOME/artifacts
 cp $HOME/artifacts/$S3_ARTIFACT_NAME.zip $HOME/$GITHUB_ARTIFACT_NAME.zip
+ls -a $HOME
