79035341eda52146bc6142420f0b35deac1c87fd
Marcin Paczkowski <dyfeer@gmail.com>
2020-10-04 20:32:03 -0700

    travis: add temporary legacy build

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -5,9 +5,12 @@ export HOMEBREW_NO_ANALYTICS=1
 brew install libsndfile || brew install libsndfile || exit 1
 brew install portaudio || exit 2
 brew install ccache || exit 3
-brew install qt5 || exit 4
-brew link qt5 --force || exit 5
-brew install fftw --verbose # temp allow
+if $IS_LEGACY_BUILD; then
+    brew install supercollider/formulae/qt@5.9.3 --force || exit 4
+else
+    brew upgrade qt5 || exit 4
+fi
+brew install fftw # temp allow
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
